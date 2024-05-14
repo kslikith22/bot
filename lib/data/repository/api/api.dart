2e91890b45bc747ab4b5d22cc29e760a1b95d184
K S Likith <kslikith2002@gmail.com>
2024-05-14 21:43:08 +0530

    code refractored

--- lib/data/repository/api/api.dart
+++ lib/data/repository/api/api.dart
@@ -2,10 +2,10 @@ import 'package:dio/dio.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class API {
-  Dio _dio = Dio();
+  final Dio _dio = Dio();
 
   API() {
-    _dio.options.baseUrl = 'http://10.0.2.2:8000/api';
+    _dio.options.baseUrl = 'http://10.0.2.2:8000/api'; // note : works for emulator only, for debugging on real device use wifi ip address
     _dio.interceptors.add(PrettyDioLogger());
   }
 
